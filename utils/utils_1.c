afb88cc9312b2e8f845d6ffc516d068f5d1d8c53
paescano <88772018+Pescano@users.noreply.github.com>
2023-11-03 18:11:02 +0100

    added window, added background, added inits & handlers

--- utils/utils_1.c
+++ utils/utils_1.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   utils_1.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: preina-g <preina-g@student.42.fr>          +#+  +:+       +#+        */
+/*   By: paescano <paescano@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/31 13:52:54 by preina-g          #+#    #+#             */
-/*   Updated: 2023/11/01 16:37:03 by preina-g         ###   ########.fr       */
+/*   Updated: 2023/11/03 18:03:29 by paescano         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,5 +37,8 @@ void	ft_free_cub3d(t_cub3d *cub3d)
 	free(cub3d->file_parser->floor);
 	free(cub3d->file_parser->ceiling);
 	free(cub3d->file_parser);
+	mlx_delete_image(cub3d->mlx, cub3d->img);
+	mlx_terminate(cub3d->mlx);
 	free(cub3d);
+	exit(EXIT_SUCCESS);
 }
